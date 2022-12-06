5441a2c48ee20d4ba2a4cf310edf9896bc165748
RajaneRahul <rahulrajane177@gmail.com>
2022-12-06 23:47:34 +0530

    code fixed for prime

--- prime.js
+++ prime.js
@@ -1,13 +1,19 @@
-function identifyPrime(num) {
-    let count=0;
-    for(let i=1;i<=num;i++){
-        if(num%i==0){
-            count++;
+function identifyPrime(number) {
+    let counter=0;
+    for(let i=1;i<=number;i++){
+        if(number%i==0){
+            counter++;
         }
     }
-    if(count==0){
-        console.log("Yes")
-    }else{
-        console.log("No")
+    if(counter==2){
+        return true;
     }
+        return false;
+    
+}
+let bag = checkprime(13);
+if(bag==true){
+console.log("prime");
+}else{
+console.log("not prime");
 }
